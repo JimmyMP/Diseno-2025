7218991ba8cde0e78da27be73a5f58afc6bbf8f5
JimmyMP <brayan.cuyate.m@uni.pe>
2025-04-23 13:21:04 -0400

    <Update>

--- Clase 8/script.js
+++ Clase 8/script.js
@@ -9,12 +9,25 @@ function carita(nota) {
         return "😭";
     }
 }
+function barrita(n){
+    let ancho= n*40;
+    return ancho;
+}
 async function notas() {
     let consulta = await fetch("https://raw.githubusercontent.com/profesorfaco/opr/refs/heads/main/clase-08/notas.json");
     let data = await consulta.json();
     console.log(data);
     data.forEach((d)=>{
-        filas.innerHTML += `<tr><td>${d.nombre}</td><td>${d.nota}</td><td>${carita(d.nota)}</td></tr>`;
+        filas.innerHTML += `<tr>
+            <td>${d.nombre}</td>
+            <td>
+            <svg xlmns="http://www.w3.org/2000/svg" viewBox="0 0 280 30">
+            <rect width="${barrita(d.nota)}" height="30" fill="${d.nota > 5.9 ? 'green' : d.nota == 5.9 ? 'yellow' : 'red'}"></rect>
+            <text x="10%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="${d.nota > 5.9 ? 'white' : d.nota == 5.9 ? 'black' : 'white'}">${d.nota}</text>
+            </svg>
+            </td>
+            <td>${carita(d.nota)}</td>
+        </tr>`;
         total += d.nota;
     });
     document.getElementById("promedio").innerHTML = (total/12).toFixed(1);
